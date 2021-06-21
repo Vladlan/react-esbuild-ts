28a67f05dc2a969080d2946415507f7c1f261a0a
Vladlan <lanvlad911@gmail.com>
2021-06-22 01:08:43 +0300

    added basic react ts app with esbuild

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useCallback} from "react";
+import ReactDOM from "react-dom";
+
+const App = (props: {message: string}) => {
+    const [count, setCount] = useState(0);
+    const increment = useCallback(() => {
+        setCount(count => count + 1);
+    }, [count]);
+    return <>
+    <h1>{props.message}</h1>
+    <h2>Count: {count}</h2>
+    <button onClick={increment}>Increment</button>
+    </>
+}
+
+ReactDOM.render(<App message="Helllo" />, document.getElementById('root'))
